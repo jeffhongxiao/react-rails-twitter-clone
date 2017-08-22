4a8b8dddda47d8d576f7cec422204f546e33b58e
Hong Xiao <hongtao.xiao@unb.ca>
2017-08-22 16:07:04 -0300

    sendTweet: Main -> TweetBox; state.tweetList: Main -> TweetList

--- app/assets/frontend/components/TweetBox.jsx
+++ app/assets/frontend/components/TweetBox.jsx
@@ -1,10 +1,20 @@
 export default class TweetBox extends React.Component {
+  sendTweet(event) {
+    event.preventDefault();
+    this.props.sendTweet(this.refs.tweetTextArea.value);
+    this.refs.tweetTextArea.value = '';
+  }
+
   render() {
     return (
       <div>
-        <h1>TweetBox</h1>
-        <textarea></textarea>
-        <button>Submit</button>
+        <form onSubmit={this.sendTweet.bind(this)}>
+          <div>
+            <textarea ref="tweetTextArea" />
+            <label>What's happening</label>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       </div>
     );
   }
--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,18 +1,26 @@
 import TweetBox from './components/TweetBox';
 import TweetList from './components/TweetList';
 
-var tweets = [
-  {id: 1, name: "Hong", body: "This is a tweet."},
-  {id: 2, name: "Hong", body: "This is another tweet."},
-  {id: 3, name: "Hong", body: "Yet another tweet."}
-];
-
 class Main extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = { tweetList: [] };
+  }
+
+  addTweet(text) {
+    let updated = this.state.tweetList;
+    const newTweet = {id: new Date(), name: 'Hong', body: text};
+    updated.unshift(newTweet);
+
+    this.setState({tweets: updated});
+  }
+
   render() {
     return (
       <div>
-        <TweetBox />
-        <TweetList tweets={tweets} />
+        <TweetBox sendTweet={this.addTweet.bind(this)} />
+        <TweetList tweets={this.state.tweetList} />
       </div>
     );
   }
