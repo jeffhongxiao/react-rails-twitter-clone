660310b0570a99570a69626353d3de7287da7378
Hong Xiao <hongtao.xiao@unb.ca>
2017-08-24 11:18:07 -0300

    add update logic in ProfileController

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -5,5 +5,12 @@ class ProfileController < ApplicationController
   end
 
   def update
+    current_user.update(user_params)
+    redirect_to root_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
   end
 end
