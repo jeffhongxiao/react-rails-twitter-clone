addfeb12659ee7cd6fee48ac75f6c913c71e08d4
hongxiao <hongtao.xiao@unb.ca>
2017-08-31 15:35:54 -0300

    display relative time in Tweet

--- app/assets/frontend/components/Tweet.jsx
+++ app/assets/frontend/components/Tweet.jsx
@@ -4,6 +4,7 @@ export default class Tweet extends React.Component {
       <li className="collection-item avatar">
         <i className="material-icons circle">person_pin</i>
         <span className="title">{this.props.name}</span>
+        <time>{moment(this.props.created_at).fromNow()}</time>
         <p>{this.props.body}</p>
       </li>
     );
