5abf6c6f09365b8f2db4909c80fffa279a7b7983
hongxiao <hongtao.xiao@unb.ca>
2017-09-01 12:44:55 -0300

    use List API with Flux: Actions and Dispatcher

---
+++ app/assets/frontend/actions/ServerActions.jsx
@@ -0,0 +1,11 @@
+import AppDispatcher from "../dispatcher";
+import ActionTypes from 'constants.jsx';
+
+export default {
+  receivedTweets(rawTweets) {
+    AppDispatcher.dispatch({
+      actionType: ActionTypes.RECEIVED_TWEETS,
+      rawTweets: rawTweets
+    });
+  }
+}
