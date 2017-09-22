2014852dab1bdb436bd216648f0896cc4a69618d
Jeff Hong Xiao <jeffhongxiao@gmail.com>
2017-09-21 21:30:42 -0300

    add /followers/random

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,6 +1,6 @@
 class FollowersController < ApplicationController
 
-  def index
-    render json: User.all
+  def random
+    render json: User.where(["id != ?", current_user.id]).order("random()").all
   end
 end
