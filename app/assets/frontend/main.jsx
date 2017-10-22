af7df044f78eb900e3f9cf613dd12fa819cc1e21
Jeff Hong Xiao <jeffhongxiao@gmail.com>
2017-10-22 15:22:16 -0300

    add Follow.jsx and use it in main.jsx

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Index from './components/Index';
+import Follow from './components/Follow';
 
 import { Router, Route, Link } from 'react-router';
 
@@ -22,6 +23,7 @@ const documentReady = () => {
       <Router>
         <Route component={App}>
           <Route path="/" component={Index} />
+          <Route path="/follow" component={Follow} />
         </Route>
       </Router>
       , reactNode);
